3c1da08ba678530f2c9da92301ade12686bfa8b9
escurse <av1967@naver.com>
2024-10-29 14:01:34 +0900

    portfolio

--- src/main/java/com/escass/portfolio/Serviecs/PofolService.java
+++ src/main/java/com/escass/portfolio/Serviecs/PofolService.java
@@ -1,17 +1,8 @@
 package com.escass.portfolio.Serviecs;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Service;
 
-@Controller
-@RequestMapping(value="")
+@Service
 public class PofolService {
-    @RequestMapping(value="/escass.com", method=RequestMethod.GET)
-    public ModelAndView getIndex() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("pofol/index");
-        return modelAndView;
-    }
+
 }
--- src/main/java/com/escass/portfolio/contorllers/PofolController.java
+++ src/main/java/com/escass/portfolio/contorllers/PofolController.java
@@ -1,7 +1,17 @@
 package com.escass.portfolio.contorllers;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-@Service
+@Controller
+@RequestMapping(value="")
 public class PofolController {
+    @RequestMapping(value="/escass.com", method= RequestMethod.GET)
+    public ModelAndView getIndex() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("pofol/index");
+        return modelAndView;
+    }
 }
