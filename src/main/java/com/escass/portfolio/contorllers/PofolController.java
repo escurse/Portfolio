001236cd08b7cfbb9530055eaf03c3ee681da0aa
escurse <av1967@naver.com>
2024-11-04 17:28:21 +0900

    portfolio

--- src/main/java/com/escass/portfolio/contorllers/PofolController.java
+++ src/main/java/com/escass/portfolio/contorllers/PofolController.java
@@ -1,5 +1,6 @@
 package com.escass.portfolio.contorllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value="")
 public class PofolController {
-    @RequestMapping(value="/escass.com", method= RequestMethod.GET)
+    @RequestMapping(value="/escass.com", method= RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getIndex() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("pofol/index");
